c85711720bc7837ad38ee8f6a8f74bdf08a822d9
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-26 14:17:35 -0700

    Changed favorite icon to png

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,7 @@ const setupFavorites = _ => {
         // Create empty local favorites
         localStorage.setItem('favorite-gifs', JSON.stringify([]))
     }
+    // <i class="fas fa-star"></i>
     // Display favorites
     favorites.forEach(({ dataset, alt }) => {
         document.querySelector('#gif-container').innerHTML = `
@@ -22,7 +23,7 @@ const setupFavorites = _ => {
                         <figure class="image">
                             <img src="${dataset.still}" alt="${alt}" class="gif" data-still="${dataset.still}" data-animated="${dataset.animated}">
                             <span class="icon favorite">
-                                <i class="fas fa-star"></i>
+                                <img src="./assets/images/favorite-star.png" alt="fav" class="favorite-img" data-value="favorite">
                             </span>
                         </figure>
                     </div>
@@ -57,7 +58,7 @@ document.querySelector('body').addEventListener('click', e => {
                                 <figure class="image">
                                     <img src="${images.original_still.url}" alt="${e.target.textContent}" class="gif" data-still="${images.original_still.url}" data-animated="${images.original.url}">
                                     <span class="icon favorite">
-                                        <i class="far fa-star"></i>
+                                    <img src="./assets/images/normal-star.png" alt="fav" class="favorite-img">
                                     </span>
                                 </figure>
                             </div>
@@ -71,32 +72,35 @@ document.querySelector('body').addEventListener('click', e => {
     if (e.target.className === 'gif') {
         e.target.src = e.target.src === e.target.dataset.still ? e.target.dataset.animated : e.target.dataset.still
     }
-    // Add gif to favorites
-    if (e.target.className === 'far fa-star' && !timeout) {
-        favorites.push({
-            dataset: e.target.parentNode.parentNode.children[0].dataset,
-            alt: e.target.parentNode.parentNode.children[0].alt
-        })
-        localStorage.setItem('favorite-gifs', JSON.stringify(favorites))
-        e.target.className = 'fas fa-star'
-        // Timeout to prevent accidental double clicking
-        timeout = true
-        setTimeout(_ => {
-            timeout = false
-        }, 100)
-    }
-    // Remove gif from favorites
-    if (e.target.className === 'fas fa-star' && !timeout) {
-        // Find gif in favorites using still url
-        let index = 0
-        favorites.forEach(({ dataset }, i) => {
-            if (e.target.parentNode.parentNode.children[0].dataset.still === dataset.still) {
-                index = i
-            }
-        })
-        favorites.splice(index, 1)
-        localStorage.setItem('favorite-gifs', JSON.stringify(favorites))
-        e.target.className = 'far fa-star'
+    // User toggles favorite status of gif
+    if (e.target.className === 'favorite-img' && !timeout) {
+        if (e.target.dataset.value) {
+            // Remove gif from favorites
+            // Find gif in favorites using url for still
+            let index = 0
+            favorites.forEach(({ dataset }, i) => {
+                if (e.target.parentNode.parentNode.children[0].dataset.still === dataset.still) {
+                    index = i
+                }
+            })
+            // Remove gif from variable and localStorage
+            favorites.splice(index, 1)
+            localStorage.setItem('favorite-gifs', JSON.stringify(favorites))
+            // Unmark gif as favorite and change icon
+            e.target.removeAttribute('data-value')
+            e.target.src = "./assets/images/normal-star.png"
+            
+        } else {
+            // Add gif to favorites
+            favorites.push({
+                dataset: e.target.parentNode.parentNode.children[0].dataset,
+                alt: e.target.parentNode.parentNode.children[0].alt
+            })
+            localStorage.setItem('favorite-gifs', JSON.stringify(favorites))
+            // Mark gif as favorite and change icon
+            e.target.dataset.value = 'favorite'
+            e.target.src = "./assets/images/favorite-star.png"
+        }
         // Timeout to prevent accidental double clicking
         timeout = true
         setTimeout(_ => {
