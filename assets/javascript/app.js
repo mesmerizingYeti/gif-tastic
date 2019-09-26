380d7d6f867aa97cd5f3f6e316e84e1b024bb194
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-09-26 00:57:09 -0700

    Added add gif btns & gifs click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,29 @@
-document.getElementById('add-btn').addEventListener('click', e => {
-    e.preventDefault()
-    document.querySelector('#btn-container').innerHTML += `<a class="button is-info search-btn">${document.querySelector('#add-input').value}</a>`
-    document.querySelector('#add-input').value = ''
+document.querySelector('body').addEventListener('click', e => {
+    // Add search button using text input
+    if (e.target.id === 'add-btn') {
+        e.preventDefault()
+        document.querySelector('#btn-container').innerHTML += `<a class="button is-info search-btn">${document.querySelector('#add-input').value}</a>`
+        document.querySelector('#add-input').value = ''
+    }
+    // Search for gifs using button name
+    if (e.target.className === 'button is-info search-btn') {
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=3SAWLXxeqLkPwaU2IUJhyQ6EAswy7DgM&q=${e.target.textContent}&limit=10&rating=pg-13`)
+            .then (r => r.json())
+            .then(({ data }) => {
+                console.log(data)
+                data.forEach(({ images }) => {
+                    console.log(images)
+                    document.querySelector('#gif-container').innerHTML += `
+                        <img src="${images.original_still.url}" alt="${e.target.textContent}" class="gif" data-still="${images.original_still.url}" data-animated="${images.original.url}">
+                    `
+                })
+            })
+            .catch(e => console.error(e))
+    }
+    // Toggle gif animation
+    if (e.target.className === 'gif') {
+        e.target.src = e.target.src === e.target.dataset.still ? e.target.dataset.animated : e.target.dataset.still
+    }
+
+    
 })
